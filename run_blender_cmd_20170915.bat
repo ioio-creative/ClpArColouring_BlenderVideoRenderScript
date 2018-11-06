b706a8cd88821828d2de658ffa402619c9c4d9bc
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-11-06 19:59:53 +0800

    remove run_blender_cmd_20170915.bat

--- run_blender_cmd_20170915.bat
+++
@@ -1,4 +0,0 @@
-echo Start Measure %Time%>>timer.txt
-blender --background CLP_0928.blend --python video_editors_render_script_v2.py -- --texture="//Texture\\CLP_ColourSheet_170908-02.jpg" --render="clp_render\\output" --startframe=1 --endframe=750 --allowsinglecore=1 --reservedcores=0 --xresolution=854 --yresolution=480 --framerate=20
-echo Stop Measure %Time%>>timer.txt
-pause
